aea1d13588f752d2918da132bf5b3f134936fe7b
Devam Chokshi <devamchokshi23@gmail.com>
2021-07-27 15:58:12 -0700

    changed fonts, better code

--- FrontEnd/src/components/SpeakerComponent/speakersection.tsx
+++ FrontEnd/src/components/SpeakerComponent/speakersection.tsx
@@ -9,28 +9,34 @@ const Speaker = () => {
     <div className = "main-section">    
     <div className = "row2">
       <div className = "person1">    
-        <img  src = {diego}  alt ="diego" style ={{width: 200, height: 200, borderRadius: 100, overflow: "hidden", borderWidth: 6, borderColor: "red"}}/>
-        <br></br>
-        <br></br>
-        <h1>Diego Granados</h1>
-        <br></br>
-        <h2>Product Manager @ Microsoft</h2>
+        <img src = {diego}  alt ="diego" style ={{width: 200, height: 200, borderRadius: 100, overflow: "hidden", borderWidth: 6, borderColor: "red"}}/>
+        <div className = "personfont">
+        <h2>Diego Granados</h2>
+        <h3>Product Manager @ Microsoft</h3>
+        </div>
+        <div className = "font">
+        <h3>"I love Disclosed!"</h3>
+        </div>
       </div>
       <div className = "person2">
         <img src = {dominic}  alt ="dominic" style ={{width: 200, height: 200, borderRadius: 100, overflow: "hidden", borderWidth: 6, borderColor: "red"}}/>
-        <br></br>
-        <br></br>
-        <h1>Dominic Antonacci</h1>
-        <br></br>
-        <h2>Business Operations Manager @ Amazon</h2>
+        <div className = "personfont">
+        <h2>Dominic Antonacci</h2>
+        <h3>Business Operations Manager @ Amazon</h3>
+        </div>
+        <div className = "font">
+        <h3>"Such a great company wow"</h3>
+        </div>
       </div>
       <div className = "person3" >
         <img src = {walt} alt ="walt" style ={{width: 200, height: 200, borderRadius: 100, overflow: "hidden", borderWidth: 6, borderColor: "red"}}/>
-        <br></br>
-        <br></br>
-        <h1>Walt Xie</h1>
-        <br></br>
-        <h2>Talent Acquisition @ Facebook</h2>
+        <div className = "personfont">
+        <h2>Walt Xie</h2>
+        <h3>Talent Acquisition @ Facebook</h3>
+        </div>
+        <div className = "font">
+        <h3>"Smart discussions"</h3>
+        </div>
       </div>
     </div>
     </div>
