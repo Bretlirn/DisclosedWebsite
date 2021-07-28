988340be966174a0b0af12490d04a7de03db0003
Devam Chokshi <devamchokshi23@gmail.com>
2021-07-27 21:12:03 -0700

    refactor pt2

--- FrontEnd/src/components/SpeakerComponent/speakersection.tsx
+++ FrontEnd/src/components/SpeakerComponent/speakersection.tsx
@@ -5,41 +5,43 @@ import walt from "../../assets/pictures/walt.jpg";
 import './speakersection.css'
 
 const Speaker = () => {
+    const QuotesBlock = ({name, title, quote}:{
+        name:string;
+        title:string;
+        quote:string;
+    }) => {
+        return(
+            <div className = 'container'>
+                    <div className = 'name'>
+                        {name}
+                    </div>
+                    <div className = 'title'>
+                        {title}
+                    </div>
+                    <div className = 'quote'>
+                        {quote}
+                    </div>
+            </div>
+        )
+    }
+
   return (
-    <div className = "main-section">    
-    <div className = "row2">
-      <div>    
-        <img className = "person" src = {diego}  alt ="diego"/>
-        <div className = "personfont">
-        <h2>Diego Granados</h2>
-        <h3>Product Manager @ Microsoft</h3>
-        </div>
-        <div className = "font">
-        <h3>"I love Disclosed!"</h3>
-        </div>
-      </div>
-      <div>
-        <img className = "person" src = {dominic}  alt ="dominic"/>
-        <div className = "personfont">
-        <h2>Dominic Antonacci</h2>
-        <h3>Business Operations Manager @ Amazon</h3>
-        </div>
-        <div className = "font">
-        <h3>"Such a great company wow"</h3>
-        </div>
-      </div>
-      <div>
-        <img className = "person" src = {walt} alt ="walt"/>
-        <div className = "personfont">
-        <h2>Walt Xie</h2>
-        <h3>Talent Acquisition @ Facebook</h3>
-        </div>
-        <div className = "font">
-        <h3>"Smart discussions"</h3>
-        </div>
-      </div>
-    </div>
-    </div>
+    <div className = "main-container">   
+   <div className = "row">
+       <div>
+           <img className = "image" src = {diego} alt = "diego"/>
+           <QuotesBlock name = "Diego Granados" title = "Product Manager @ Microsoft" quote = '"I love disclosed"'/>
+       </div>
+       <div>
+           <img className = "image" src = {dominic} alt = "dominic"/>
+           <QuotesBlock name = "Dominic Antonacci" title = "Business Operations Manager @ Amazon" quote = '"Such a great company wow"'/>
+       </div>
+       <div>
+           <img className = "image" src = {walt} alt = "walt"/>
+           <QuotesBlock name = "Walt Xie" title = "Talent Acquisition @ Facebook" quote = '"Smart discussions"'/>
+       </div>
+   </div>
+   </div>
   );
 }
 export default Speaker;
