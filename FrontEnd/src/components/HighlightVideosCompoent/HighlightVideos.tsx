b2f40bf816d5725ba1d8ec1392f4f19a20b52a2e
Bretlirn <vinsonchen222@gmail.com>
2021-08-09 13:04:45 -0700

    Fixed css problems

--- FrontEnd/src/components/HighlightVideosCompoent/HighlightVideos.tsx
+++ FrontEnd/src/components/HighlightVideosCompoent/HighlightVideos.tsx
@@ -10,18 +10,18 @@ const HighlightVideos = () =>{
                 <div className = "highlight-videos-flexbox-left">
                     <div className = "highlight-video-left">
                         <div className = "highlight-video-picture">
-                            <iframe width="420" height="237" src="https://www.youtube.com/embed/uiZk61W36oo" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+                        <iframe width="420" height="237" src="https://www.youtube.com/embed/W_pCd9Bpl-I" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" />
                         </div>                        
                         <div className = "highlight-video-text-left">
-                            Person's Name 1
+                            STAND OUT to a Genentech Recruiter
                         </div>
                     </div>                    
                     <div className = "highlight-video-left">
                         <div className = "highlight-video-picture">
-                        <iframe width="420" height="237" src="https://www.youtube.com/embed/uiZk61W36oo" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+                        <iframe width="420" height="237" src="https://www.youtube.com/embed/YgRCA-xWSlQ" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" />
                         </div>                        
                         <div className = "highlight-video-text-left">
-                            Person's Name 2
+                            Jordan Worley, Business Program Manager @ Microsoft
                         </div>
                     </div>
                 </div>
@@ -29,10 +29,10 @@ const HighlightVideos = () =>{
                 <div className = "highlight-videos-flexbox-right">
                     <div className = "highlight-video-right">
                         <div className = "highlight-video-text-right">
-                            Person's Name 3
+                            James Helms, VP of Design and Product @ Intuit
                         </div>
                         <div className = "highlight-video-picture">
-                            <iframe width="420" height="237" src="https://www.youtube.com/embed/uiZk61W36oo" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+                            <iframe width="420" height="237" src="https://www.youtube.com/embed/ujuEgpR5nus" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
                         </div>
                     </div>
                 </div>
--- FrontEnd/src/components/MakersComponent/Makers.tsx
+++ FrontEnd/src/components/MakersComponent/Makers.tsx
@@ -13,33 +13,33 @@ const Makers = () => {
     }) => {
         return(
             <div className = 'container'>
-                    <img src = {image} alt = "Not found" className = "image"/>
-                    <div className = 'name'>
-                        {name}
-                    </div>
-                    <div className = 'description'>
-                        {description}
-                    </div>
+                <img src = {image} alt = "Not found" className = "image"/>
+                <div className = 'name'>
+                    {name}
+                </div>
+                <div className = 'description'>
+                    {description}
+                </div>
             </div>
         )
     }
 
   return (
-  <div className = "main-container">   
-    <div className = "makerheader">
+    <div className = "main-container">
+        <div className = "makerheader">
             <strong>
-            <p>Meet the Makers</p>
-            </strong>       
+                <p>Meet the Makers</p>
+            </strong>
+        </div>
+        <div className = "founder">
+            <MakerBlock image = {michelle} name = "Michelle Chen" description = "Founder of Disclosed"/>
+        </div>
+        <div className = "leads">
+            <MakerBlock image = {vinson} name = "Vinson Chen" description = "Technology Systems Lead"/>
+            <MakerBlock image = {fiona} name = "Fiona Li" description = "Marketing Lead"/>
+            <MakerBlock image = {addison} name = "Addison Chen" description = "Partnership Lead"/>
+        </div>
     </div>
-   <div className = "founder">
-       <MakerBlock image = {michelle} name = "Michelle Chen" description = "Founder of Disclosed"/>
-   </div>
-   <div className = "leads">
-       <MakerBlock image = {vinson} name = "Vinson Chen" description = "Technology Systems Lead"/>
-       <MakerBlock image = {fiona} name = "Fiona Li" description = "Marketing Lead"/>
-       <MakerBlock image = {addison} name = "Addison Chen" description = "Partnership Lead"/>
-   </div>
-  </div>
   );
   
 }
